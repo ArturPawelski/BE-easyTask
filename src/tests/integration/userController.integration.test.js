02e795ea13f7bd2889df959cd582f7c0c6472cce
ArturPawelski <arturpawelski13@gmail.com>
2024-04-22 21:08:52 +0200

    unit test and integration fix

--- src/tests/integration/userController.integration.test.js
+++ src/tests/integration/userController.integration.test.js
@@ -2,35 +2,34 @@ const request = require('supertest');
 const mongoose = require('mongoose');
 const express = require('express');
 const userRouter = require('../../routes/userRoutes');
+const bcrypt = require('bcrypt');
 
 describe('User Controller Integration Tests', () => {
   const app2 = express();
   app2.use(express.json());
   app2.use('/users', userRouter);
 
-  beforeAll(() => {
+  beforeAll(async () => {
+    if (mongoose.connection.readyState) {
+      await mongoose.disconnect();
+    }
+    await mongoose.connect(process.env.MONGO_TESTING_DB_CONNECT);
+
     server = app2.listen(0, () => {
       console.log('test server runs');
     });
   });
 
-  beforeEach(async () => {
-    if (mongoose.connection.readyState) {
-      await mongoose.disconnect();
-    }
-    await mongoose.connect(process.env.MONGO_TESTING_DB_CONNECT);
-  });
-
-  afterEach(async () => {
+  afterAll(async () => {
     await mongoose.connection.dropDatabase();
     await mongoose.disconnect();
-  });
 
-  afterAll((done) => {
-    server.close(() => {
-      console.log('Server closed');
-      done();
-    });
+    await new Promise((resolve) =>
+      server.close(() => {
+        console.log('Server closed successfully');
+        resolve();
+      })
+    );
   });
 
   it('should register a new user', async () => {
@@ -46,5 +45,25 @@ describe('User Controller Integration Tests', () => {
     expect(res.body.success).toEqual(true);
   });
 
-  it('should login an existing user', async () => {});
+  it('should login a user and send http only token ', async () => {
+    await mongoose.model('Users').create({
+      name: 'ExistingUser',
+      email: 'existing@example.com',
+      password: await bcrypt.hash('passwordExample', 10),
+      isVerified: true,
+    });
+
+    await mongoose.model('Users').updateOne({ email: 'existing@example.com' }, { $set: { isVerified: true } });
+
+    const loginRes = await request(server).post('/users/login').send({
+      email: 'existing@example.com',
+      password: 'passwordExample',
+    });
+
+    expect(loginRes.statusCode).toEqual(200);
+    expect(loginRes.body.success).toEqual(true);
+
+    const cookies = loginRes.headers['set-cookie'];
+    expect(cookies).toEqual(expect.arrayContaining([expect.stringMatching(/^accessToken=.+;.*HttpOnly/)]));
+  });
 });
---
+++ src/tests/unit/controllers/userController.test.js
@@ -0,0 +1,68 @@
+const UserService = require('../../../services/user/userService');
+const userDataService = require('../../../services/user/userDataService');
+const { registerUser } = require('../../../controllers/userController');
+const userService = require('../../../services/user/userService');
+
+jest.mock('../../../services/user/userService');
+jest.mock('../../../services/user/userDataService');
+jest.mock('../../../services/responseDTO', () =>
+  jest.fn().mockImplementation((success, data, message) => ({
+    success,
+    data,
+    message,
+  }))
+);
+
+describe('UserController', () => {
+  describe('registerUser', () => {
+    let req, res;
+    beforeEach(() => {
+      req = {
+        body: {
+          name: 'TestUser',
+          email: 'test@example.com',
+          password: 'password123',
+        },
+      };
+      res = {
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis(),
+      };
+      jest.clearAllMocks();
+    });
+
+    it('should register a new user successfully', async () => {
+      userService.registerUser.mockResolvedValue({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      await registerUser(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(201);
+      expect(res.json).toHaveBeenCalledWith({
+        success: true,
+        data: null,
+        message: 'registration completed successfully',
+      });
+    });
+
+    it('should handle errors when registration fails', async () => {
+      const error = new Error('User already registered');
+      error.statusCode = 409;
+
+      userService.registerUser.mockRejectedValue(error); // simulate an error
+      await registerUser(req, res);
+
+      // console.log('JSON called with:', res.json.mock.calls);
+
+      expect(res.status).toHaveBeenCalledWith(409);
+      expect(res.json).toHaveBeenCalledWith({
+        success: false,
+        data: null,
+        message: 'User already registered',
+      });
+    });
+  });
+});
